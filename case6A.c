6f650dcdeca67907154da48809492a7265d6c7a7
s6804062612293 <s6804062612293@email.kmutnb.ac.th>
2025-12-16 13:51:27 +0700

    Bonus1 คะแนนไม่อยู่ระหว่าง 0-100 แจ้ง error score

--- case6A.c
+++ case6A.c
@@ -3,20 +3,24 @@
 int main(){
 	int score;
 	scanf("%d",&score);
-	if (score>= 68){
-		if (score>=80)
-			printf("A");
-		else if(score>=75)
-			printf("B");
-		else
-			printf("C");		
+	if(score>=0&&score<=100){
+		if (score>= 68){
+			if (score>=80)
+				printf("A");
+			else if(score>=75)
+				printf("B");
+			else
+				printf("C");		
+		}
+		else {
+			if (score>=55)
+				printf("D");
+			else
+				printf("F");
+		}
 	}
-	else {
-		if (score>=55)
-			printf("D");
-		else
-			printf("F");
-	}
-		
+	else
+		printf("error score");
 	return 0;
 }
+
